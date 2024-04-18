6dc4e293e915b2adc5ac091c898966cceb62f763
karim <arfaouikarim77@gmail.com>
2024-04-18 23:16:16 +0100

    article page

--- src/app/Config-pages/article/article.component.ts
+++ src/app/Config-pages/article/article.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DropdownModule } from 'primeng/dropdown';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
+ 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [InputNumberModule,ButtonModule,DropdownModule,
+    RouterLink,FormsModule,CheckboxModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
 export class ArticleComponent {
-
+  cites: string[]=['fkarim','arfakjrh','new yourk'];
+  selectedCity: string[] = ['lmdkfqjlkj'];
+showClear: any;
+ Datedejour: Date=new Date();
 }
