c237a20aef4e0bc57f034fae48fa09542a98b1c4
Aziz <soltaniaziz202@gmail.com>
2024-04-24 22:19:52 +0100

    updates

--- src/app/Config-pages/article/article.component.spec.ts
+++ src/app/Config-pages/article/article.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArticleComponent } from './article.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ArticleComponent', () => {
   let component: ArticleComponent;
@@ -8,7 +9,7 @@ describe('ArticleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ArticleComponent]
+      imports: [ArticleComponent, HttpClientModule]
     })
     .compileComponents();
     
--- src/app/Config-pages/article/article.component.ts
+++ src/app/Config-pages/article/article.component.ts
@@ -1,22 +1,55 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, inject } from '@angular/core';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { DropdownModule } from 'primeng/dropdown';
 import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
- 
+import { ArticleService } from '../../@services/article.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { environment } from '../../../env/env';
+
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [InputNumberModule,ButtonModule,DropdownModule,
-    RouterLink,FormsModule,CheckboxModule],
+  imports: [InputNumberModule, ButtonModule, DropdownModule,
+    RouterLink, FormsModule, CheckboxModule,CommonModule, HttpClientModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
-export class ArticleComponent {
-  cites: string[]=['fkarim','arfakjrh','new yourk'];
-  selectedCity: string[] = ['lmdkfqjlkj'];
-showClear: any;
- Datedejour: Date=new Date();
-}
+export class ArticleComponent implements OnInit{
+  httpClient = inject(HttpClient);
+  article: any[]= [] ;
+
+  ngOnInit(): void {
+    this.fetchArticle();
+    
+  }
+
+  fetchArticle(){
+    this.httpClient
+    .get(`${environment.apiUrl}/article`)
+    .subscribe((article: any) => {
+      this.article = article;
+      console.log('article', article);
+      this.article = article;
+    });
+}}
+
+  /*constructor(
+    private articleService: ArticleService,
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.getArticle().subscribe(res => {
+      console.log(res)
+    })
+
+
+  }
+  getArticle(): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/article`);
+  }
+}*/
